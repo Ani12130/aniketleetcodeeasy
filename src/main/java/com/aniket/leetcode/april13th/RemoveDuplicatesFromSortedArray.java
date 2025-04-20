33f3c9c5e52645d68d9fbcd9ff8edc1403244549
Ani12130 <aniparanjpe3@gmail.com>
2025-04-20 20:33:28 +0530

    Printing Removed Duplicates
    Printing Removed Duplicates From array

--- src/main/java/com/aniket/leetcode/april13th/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/aniket/leetcode/april13th/RemoveDuplicatesFromSortedArray.java
@@ -1,6 +1,8 @@
 package com.aniket.leetcode.april13th;
 
 import java.util.Arrays;
+import java.util.HashMap;
+
 //Question Link :https://leetcode.com/problems/remove-duplicates-from-sorted-array?envType=study-plan-v2&envId=top-interview-150
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
@@ -9,11 +11,33 @@ public class RemoveDuplicatesFromSortedArray {
        int[] nums1 = new int[]{0,0,1,1,1,2,2,3,3,4};
         System.out.println("Removed Duplicates From Sorted Array and length of expected array" + " " + rd.removeDuplicates(nums));
         System.out.println("Removed Duplicates From Sorted Array and length of expected array" + " " + rd.removeDuplicates(nums1));
+        System.out.println("Print Removed Duplicates from nums array");
+        printRepeatedIntegers(nums);
+        System.out.println("Print Removed Duplicates From nums 1 array");
+        printRepeatedIntegers(nums1);
+
     }
     public int removeDuplicates(int[] nums) {
         int [] k = Arrays.stream(nums).sorted().distinct().toArray();
        System.out.println(Arrays.toString(k));
        return k.length;
 
+    }
+    public static void printRepeatedIntegers(int[] array) {
+        // Create a HashMap to store the frequency of each integer
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+
+        // Populate the frequency map
+        for (int num : array) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Print integers that appear more than once
+        System.out.println("Repeated integers:");
+        for (HashMap.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " (Repeated " + entry.getValue() + " times)");
+            }
+        }
     }
 }
--- src/test/java/com/aniket/leetcode/april13th/RemoveDuplicatesFromSortedArrayTest.java
+++ src/test/java/com/aniket/leetcode/april13th/RemoveDuplicatesFromSortedArrayTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class RemoveDuplicatesFromSortedArrayTest {
     RemoveDuplicatesFromSortedArray rd = new RemoveDuplicatesFromSortedArray();
@@ -15,5 +16,12 @@ public class RemoveDuplicatesFromSortedArrayTest {
         int[] nums1 = new int[]{0,0,1,1,1,2,2,3,3,4};
         assertEquals(2, rd.removeDuplicates(nums));
         assertEquals(5,rd.removeDuplicates(nums1));
+    }
+    @Test
+    public void printDuplicatesTest() {
+        int[] nums = new int[]{1, 1, 2};
+        int[] nums1 = new int[]{0,0,1,1,1,2,2,3,3,4};
+        RemoveDuplicatesFromSortedArray.printRepeatedIntegers(nums);
+        RemoveDuplicatesFromSortedArray.printRepeatedIntegers(nums1);
     }
 }
